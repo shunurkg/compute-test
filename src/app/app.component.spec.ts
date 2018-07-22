8dc5f84f7e6d0a9b9715fedb214db03b181d2678
shunmathi <shunu17796@gmail.com>
2018-07-23 01:37:17 +0530

    compute test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,39 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'compute test'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('compute test');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to calculator!');
+    expect(compiled.querySelector('h1').textContent).toContain('compute test!');
   }));
+  beforeEach(() => { 
+    this.objCompute = new AppComponent();
+   });
+// afterEach(() => { 
+//     this.objCaculator=null;
+//     this.firstNumber=0;
+//     this.secondNumber=0;
+//     this.result=0;
+// });
+it('check fibonacci series', () => {
+  var result=this.objCompute.FibonnaciSeries(5);
+  console.log(result);
+  expect(result).toString();
+});
+it('should find the lcm and check if its greater to any given number', () => {
+  var result=this.objCompute.LCM(72,120);
+  console.log(result);
+  expect(result).toBeGreaterThan(72);
+});
+it('should check if the lcm is greater than or equal to the largest number', () => {
+  var result=this.objCompute.LCM(2,4);
+  console.log(result);
+  expect(result).toBeGreaterThanOrEqual(4);
+});
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'compute test';
+  FibonnaciSeries(number) {
+    var first = 0;
+    var second = 1;
+    var resultant;
+    var series = "0,1";
+    if (number == 1)
+      series = "0";
+    else if (number == 2) {
+    }
+    else {
+      for (var i = 0; i < number - 2; i++) {
+        resultant = first + second;
+        first = second;
+        second = resultant;
+        series.concat("," + resultant);
+      }
+    } console.log(series);
+    return series;
+  }
+
+  LCM(n1, n2) {
+    var lcm;
+    lcm = (n1 > n2) ? n1 : n2;
+    while (true) {
+      if (lcm % n1 == 0 && lcm % n2 == 0) {
+        return lcm;
+        break;
+      }
+      ++lcm;
+    } return lcm;
+  }
 }
